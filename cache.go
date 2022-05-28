80eb9b088f51ac430dfe30831df67a7d8075a750
Ivan Victorov <victorov.i.vl@yandex.ru>
2022-05-28 22:45:34 +0300

    Added project

--- cache.go
+++ cache.go
@@ -22,17 +22,45 @@ func NewCache() Cache {
 }
 
 func (cache *Cache) Get(key string) (string, bool) {
-	return "", true
+	cache.mu.Lock()
+	cache.mu.Unlock()
+
+	if _, ok := cache.data[key]; !ok {
+		return "", false
+	}
+
+	if cache.data[key].deadline != nil && cache.data[key].deadline.Before(time.Now()) {
+		return "", false
+	}
+
+	return cache.data[key].value, true
 }
 
 func (cache *Cache) Put(key, value string) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = Data{value, nil}
 }
 
 func (cache *Cache) Keys() []string {
+	cache.mu.Lock()
+	cache.mu.Unlock()
 	var keys []string
+	now := time.Now()
+	for key, value := range cache.data {
+		if value.deadline != nil && value.deadline.Before(now) {
+			break
+		}
+		keys = append(keys, key)
+	}
+
 	return keys
 }
 
 func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.mu.Lock()
+	cache.mu.Unlock()
+
 	cache.data[key] = Data{value, &deadline}
 }
